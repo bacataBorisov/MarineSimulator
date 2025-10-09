ee66001ce1718f74f43b5457c2d12dcc91b10eab
bacataBorisov <vasil.borisovv@gmail.com>
2025-10-08 21:24:14 -0300

    Add assets/media folders and screenshots

--- MarineSimulatorUITests/MarineSimulatorUITests.swift
+++ MarineSimulatorUITests/MarineSimulatorUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class NMEASimulatorUITests: XCTestCase {
+final class MarineSimulatorUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- MarineSimulatorUITests/MarineSimulatorUITestsLaunchTests.swift
+++ MarineSimulatorUITests/MarineSimulatorUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class NMEASimulatorUITestsLaunchTests: XCTestCase {
+final class MarineSimulatorUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
